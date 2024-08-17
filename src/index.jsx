3649c2c5692a9fe2445b2e32c96e956cab2112f1
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-17 11:24:49 -0700

    completed challenge

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+// 4 javascript react practice
 //Create a react app from scratch.
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
@@ -7,8 +8,19 @@
 //Created by Angela Yu.
 //Copyright 2019.
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
+import React from "react"; 
+import ReactDOM from "react-dom/client"; 
+
+const fname = "Jonnii";
+const lname = "ringo"; 
+const currentYear = new Date().getFullYear(); 
+
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+
+root.render(
+    <div>
+        <p>Created by {fname + " " + lname}</p>
+        <p>Copyright {currentYear}</p>
+    </div>
+);
